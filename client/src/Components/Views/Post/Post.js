122429731a42e4d094f55a5ee93da31c4bf02b1b
Seung Hyun Tark <makepin2r@gmail.com>
2020-09-13 10:28:43 +0900

    Post 작업중

--- client/src/Components/Views/Post/Post.js
+++ client/src/Components/Views/Post/Post.js
@@ -9,7 +9,31 @@ class Post extends Component {
             <Fragment>
                 <PostHeader title={title}/>
                 <div className="post-container">
-                    <div className="container flex flex-between-v"></div>
+                    <div className="container flex flex-between-v">
+                        <div className="title-box">
+                            <h1>제목 없음</h1>
+                            <p className="sub-title">부제목</p>
+                            <div className="profile-box">
+                                <img src="" alt="user profile"/>
+                                <div className="text-wrap">
+                                    <p>안녕로봇</p>
+                                    <date>2020.09.09</date>
+                                    <span>댓글 0</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="content-box">
+                        가 나 다 라 마 바 사 아 자 차 카 타 파 하 <br/>
+                        <br/>
+                        가 나 다 라 마 바 사 아 자 차 카 타 파 하 <br/>
+                        <br/>
+                        가 나 다 라 마 바 사 아 자 차 카 타 파 하 <br/>
+                        <br/>
+                        가 나 다 라 마 바 사 아 자 차 카 타 파 하 <br/>
+                        <br/>
+                        가 나 다 라 마 바 사 아 자 차 카 타 파 하 
+                        </div>
+                    </div>
                 </div>
                 <Footer title={title}/>
             </Fragment>
--- client/src/Components/Views/Post/PostHeader.js
+++ client/src/Components/Views/Post/PostHeader.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import {Link} from 'react-router-dom'
+import ImgBtnSearch from '../../../images/ico-search.png'
 
 class PostHeader extends Component {
     render() {
@@ -10,20 +11,29 @@ class PostHeader extends Component {
                     <div className="gnb-container">
                         <div>
                             <span className="logo-box"><Link to='/'>홈으로</Link></span>
-                            <h1>{title}</h1>
+                            <p className="postype-title">{title}</p>
                         </div>
                         <div>
                             <ul className="icon-list">
                                 <li className="search">검색</li>
-                                <li>알림</li>
-                                <li>마이페이지</li>
+                                <li className="noti">알림</li>
+                                <li className="mypage">마이페이지</li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div className="post-nav-box">
                     <div className="gnb-container">
-
+                        <button className="btn-hover home">홈으로 가기</button>
+                        <div className="nav-wrap">
+                            <button className="btn-hover prev-article">이전글</button>
+                            <p className="article-title">제목 없음</p>
+                            <button className="btn-hover next-article">다음글</button>
+                        </div>
+                        <ul className="icon-list">
+                                <li className="bookmark">북마크하기</li>
+                                <li className="settings">설정</li>
+                            </ul>
                     </div>
                 </div>
             </Fragment>
